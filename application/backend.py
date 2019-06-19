446f4fbe229b321240e9047cb36f5c70b85de20d
Sean Shanahan <seanmodg@gmail.com>
2019-06-19 10:44:53 -0400

    Added named parameterization to the SQL statements

--- application/backend.py
+++ application/backend.py
@@ -4,20 +4,18 @@ class Connector:
 
     def __init__(self,name):
         self.db_name = name
-        self.table = "book"
 
-    def change_table(self,table):
-        self.table = table
-
-    def create_db(self):
-        command = "CREATE TABLE IF NOT EXISTS ? (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)", (self.table)
+    def create_table(self): 
+        command = "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)"
         return self.generic_connector(command)
 
     def search(self,title="",author="",year="",isbn=""):
-        command = "SELECT * FROM ? WHERE title=? OR author=? OR year=? OR isbn=?", (self.table,title,author,year,isbn)
+        command = "SELECT * FROM book WHERE title=:title OR author=:author OR year=:year OR isbn=:isbn", {"title": title,"author": author,"year": year,"isbn":isbn}
         return self.generic_connector(command) 
 
     def generic_connector(self,command):
+        #command = 
+        print(command)
         try:
             conn = sqlite3.connect(self.db_name)
             cur = conn.cursor()
@@ -26,29 +24,30 @@ class Connector:
             conn.commit()
             conn.close()
             return rows
-        except ValueError:
-            pass
-        
+        except (ValueError, SyntaxError):
+            print("oops")
 
-    def insert(self,title,author,isbn,year):
+
+    def insert(self,title,author,year,isbn):
         try:
             title = str(title)
             author = str(author)            
             isbn = int(isbn)
             year = int(year)
-            command = "INSERT INTO ? VALUES(?,?,?,?)", (self.table,title,author,isbn,year) 
+            command = "INSERT INTO book VALUES(:title,:author,:year,:isbn)", {"title": title,"author": author,"year": year,"isbn":isbn} 
+            print(command)
             return self.generic_connector(command)
         except ValueError:
-            pass
+            return 1
         
         
-    def delete(self, id):
+    def delete(self,id):
         try:
             id = int(id)
-            command = "DELETE FROM ? WHERE id=?", (self.table,id)
+            command = "DELETE FROM book WHERE id=:id", {"id": id}
             return self.generic_connector(command)
         except ValueError:
-            pass
+            return 1
         
 
     def update(self,id,title,author,isbn,year):
@@ -57,12 +56,17 @@ class Connector:
             author = str(author)
             isbn = int(isbn)
             year = int(year)
-            command = "UPDATE ? SET title=?, author=?, year=?, isbn=? WHERE id=?", (self.table,title,author,isbn,year,id)
+            command = "UPDATE book SET title=:title, author=:author, year=:year, isbn=:isbn WHERE id=:id", {"title": title,"author": author,"year": year,"isbn":isbn,"id": id}
             return self.generic_connector(command)
         except ValueError:
-            pass
+            return 1
           
 
     def view(self):
-        command = "SELECT * FROM ?", (self.table)
+        command = "SELECT * FROM book"
         return self.generic_connector(command)
+
+link = Connector("books.db")
+link.create_table()
+link.insert("Clean Code in Python","Mariano Anaya", 2018, 1788835832)
+print(link.view())
